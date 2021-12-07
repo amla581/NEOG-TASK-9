3682ebea9ef9d07c9792855ae9e345c6a45d58e6
amla581 <amla21@navgurukul.org>
2021-12-07 20:52:19 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
